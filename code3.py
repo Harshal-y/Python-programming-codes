c8ba3ab938a3f5b1321681e5b1b8bc5d1ca4251e
Harshal Y.y <76250999+Harshal-y@users.noreply.github.com>
2025-01-21 10:49:38 +0500

    Updated-one code3.py

--- code3.py
+++ code3.py
@@ -1,5 +1,5 @@
 year=int(input("Enter a year: "))
-if (year%4==0) and (year%100==0) and (year%400==0):
+if (year%4==0) or (year%100==0) and (year%400==0):
     print("It is a leap year.")
 elif (year%100==0) and (year%400!=0):
     print("Not a leap year.")
