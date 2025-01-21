8b663c2233eabe41753cc9932f9089e8785b4475
Harshal Y.y <76250999+Harshal-y@users.noreply.github.com>
2025-01-21 10:28:29 +0500

    Updated-second code1.py

--- code1.py
+++ code1.py
@@ -2,5 +2,3 @@ name=input("Enter your name: ")
 age=int(input("Enter your age: "))
 salary=int(input("Enter your salary: "))
 print("Hello",name,"did you know your name is a",type(name),"and your age is",age,"which is an",type(age),"your salary is",salary,"and you are a True person and your class is",type(True))
-for i in range(1,10,2):
-    print(i)
