6d405bb2e7b4902f87f8b016516e0752e3adf507
Harshal Y.y <76250999+Harshal-y@users.noreply.github.com>
2025-01-21 10:10:16 +0500

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+salary=int(input("Enter your salary: "))
+print("Hello",name,"did you know your name is a",type(name),"and your age is",age,"which is an",type(age),"your salary is",salary)
+for i in range(1,10,2):
+    print(i)
